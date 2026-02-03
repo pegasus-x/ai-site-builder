2c3559010407e3aa1d6d8d5a5aa25bd48d8166db
Rati Ranjan Sendha <135224384+pegasus-x@users.noreply.github.com>
2026-02-03 22:28:08 +0530

    Refactor Navbar component and add session handling

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, useNavigate } from "react-router-dom";
-import { authClient } from "@/lib/auth-client";
+import { useSession } from "@/lib/auth-client";
 import { UserButton } from "@daveyplate/better-auth-ui";
 import api from "@/configs/axios";
 import { toast } from "sonner";
@@ -177,6 +177,7 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
 
 
 

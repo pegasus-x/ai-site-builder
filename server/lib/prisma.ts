03185e8b5a04e85af1a2c301957760ff029cdd2e
Rati Ranjan Sendha <135224384+pegasus-x@users.noreply.github.com>
2026-02-04 17:36:58 +0530

    Update import path for PrismaClient

--- server/lib/prisma.ts
+++ server/lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "@prisma/client";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -15,4 +15,5 @@ const prisma = new PrismaClient({
 });
 
 export default prisma;
+
 

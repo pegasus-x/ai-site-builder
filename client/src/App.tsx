53a7ce68f19eb8bf6d599406cc0e19560e0daf5e
Rati Ranjan Sendha <135224384+pegasus-x@users.noreply.github.com>
2026-02-11 23:56:16 +0530

    Update import path for trackPageView function

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ import { Toaster} from "sonner";
 import AuthPage from "./pages/auth/AuthPage";
 import  Settings  from "./pages/Settings";
 import Loading from "./pages/Loading";
-import { trackPageView } from "./analytics";
+import { trackPageView } from "./analytics.js";
 
 // GA Tracking
  useEffect(() => {
